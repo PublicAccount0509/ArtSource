6c46622b2b87a0b84fa95533d29b4719e600e0a2
zhouchao <zhou.tao@etaoshi.com>
2013-10-22 17:33:52 +0800

    add order

---
+++ Ets.SingleApi.Controllers/Wap/WapOrdersController.cs
@@ -0,0 +1,18 @@
+﻿namespace Ets.SingleApi.Controllers.Wap
+{
+    using System.Web.Http;
+
+    /// <summary>
+    /// 类名称：WapOrdersController
+    /// 命名空间：Ets.SingleApi.Controllers.Wap
+    /// 类功能：订单功能
+    /// </summary>
+    /// 创建者：周超
+    /// 创建日期：10/22/2013 5:20 PM
+    /// 修改者：
+    /// 修改时间：
+    /// ----------------------------------------------------------------------------------------
+    public class WapOrdersController : ApiController
+    {
+    }
+}
