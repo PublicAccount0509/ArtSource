5d462c4b9f7b40f3d3d38fe8ed11916ca5132886
zhouchao <zhou.tao@etaoshi.com>
2013-10-28 18:09:38 +0800

    add test

---
+++ Ets.SingleApi.Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿namespace Ets.SingleApi.Controllers
+{
+    using System.Web.Http;
+
+    using Ets.SingleApi.Model.Controller;
+
+    /// <summary>
+    /// 类名称：TestController
+    /// 命名空间：Ets.SingleApi.Controllers
+    /// 类功能：测试
+    /// </summary>
+    /// 创建者：周超
+    /// 创建日期：10/28/2013 6:03 PM
+    /// 修改者：
+    /// 修改时间：
+    /// ----------------------------------------------------------------------------------------
+    public class TestController : SingleApiController
+    {
+        /// <summary>
+        /// 测试方法
+        /// </summary>
+        /// <returns>
+        /// The ApiResponse
+        /// </returns>
+        /// 创建者：周超
+        /// 创建日期：10/28/2013 6:04 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        [HttpGet]
+        public ApiResponse Test()
+        {
+            return new ApiResponse();
+        }
+    }
+}
