6010e7b99454cb2881b8e03238cf59043a73d361
zhouchao <zhou.tao@etaoshi.com>
2013-12-20 09:20:19 +0800

    add

--- Ets.SingleApi.Hosting/Contracts/IOrdersService.cs
+++ Ets.SingleApi.Hosting/Contracts/IOrdersService.cs
@@ -42,7 +42,6 @@
         /// </summary>
         /// <param name="id">订单号</param>
         /// <param name="orderType">订单类型：0 外卖，1 堂食，2 订台</param>
-        /// <param name="orderSourceType">订单来源：0 默认类型，1 海底捞；默认为0</param>
         /// <returns>
         /// 返回结果
         /// </returns>
@@ -53,7 +52,24 @@
         /// ----------------------------------------------------------------------------------------
         [OperationContract]
         [Description("方法功能：取得外卖订单详情；参数说明：id（订单号），orderType（订单类型：0 外卖，1 堂食，2 订台），orderSourceType（订单来源：0 默认类型，1 海底捞；默认为 0）")]
-        Response<WaiMaiOrderDetailModel> GetOrder(string id, int orderType, int orderSourceType);
+        Response<WaiMaiOrderDetailModel> GetOrder(string id, int orderType);
+
+        /// <summary>
+        /// 取得外卖订单详情
+        /// </summary>
+        /// <param name="id">订单号</param>
+        /// <param name="orderType">订单类型：0 外卖，1 堂食，2 订台</param>
+        /// <returns>
+        /// 返回结果
+        /// </returns>
+        /// 创建者：周超
+        /// 创建日期：10/23/2013 9:26 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        [OperationContract]
+        [Description("方法功能：取得海底捞外卖订单详情；参数说明：id（订单号），orderType（订单类型：0 外卖，1 堂食，2 订台），orderSourceType（订单来源：0 默认类型，1 海底捞；默认为 0）")]
+        Response<HaiDiLaoWaiMaiOrderDetailModel> GetHaiDiLaoOrder(string id, int orderType);
 
         /// <summary>
         /// 保存订单信息
