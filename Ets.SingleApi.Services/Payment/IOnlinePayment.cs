aed819407bc701a864d51c7635387fd53d8347e2
wangwei <wang.wei@etaoshi.com>
2014-02-28 16:11:08 +0800

    modify payment

--- Ets.SingleApi.Services/Payment/IOnlinePayment.cs
+++ Ets.SingleApi.Services/Payment/IOnlinePayment.cs
@@ -12,5 +12,32 @@
     /// ----------------------------------------------------------------------------------------
     public interface IOnlinePayment
     {
+        /// <summary>
+        /// 获取支付Url地址
+        /// </summary>
+        /// <param name="parameter">The parameterDefault documentation</param>
+        /// <returns>
+        /// 支付Url地址
+        /// </returns>
+        /// 创建者：王巍
+        /// 创建日期：2/28/2014 4:08 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        PaymentResult<string> GetPaymentUrl(IPaymentData parameter);
+
+        /// <summary>
+        /// 查询订单支付状态
+        /// </summary>
+        /// <param name="parameter">The parameterDefault documentation</param>
+        /// <returns>
+        /// 是否已支付
+        /// </returns>
+        /// 创建者：王巍
+        /// 创建日期：2/28/2014 4:08 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        PaymentResult<bool> QueryState(IPaymentData parameter);
     }
 }
