a2b5c88acafab9b1c06593d7e2f5375500932f70
wangwei <wang.wei@etaoshi.com>
2014-02-28 16:05:13 +0800

    add IOnlinePayment

---
+++ Ets.SingleApi.Services/Payment/IOnlinePayment.cs
@@ -0,0 +1,16 @@
+﻿namespace Ets.SingleApi.Services.Payment
+{
+    /// <summary>
+    /// 接口名称：IOnlinePayment
+    /// 命名空间：Ets.SingleApi.Services.Payment
+    /// 接口功能：在线支付接口
+    /// </summary>
+    /// 创建者：王巍
+    /// 创建日期：2/28/2014 4:04 PM
+    /// 修改者：
+    /// 修改时间：
+    /// ----------------------------------------------------------------------------------------
+    public interface IOnlinePayment
+    {
+    }
+}
