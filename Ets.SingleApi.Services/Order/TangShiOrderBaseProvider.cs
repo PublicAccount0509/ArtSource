5f394a0581fdf78186e010d02ee6c425a142d8e2
liushengli <liu.shengli@etaoshi.com>
2014-02-28 16:26:36 +0800

    Modify 新增订单BaseOrder实现

---
+++ Ets.SingleApi.Services/Order/TangShiOrderBaseProvider.cs
@@ -0,0 +1,143 @@
+﻿using System.Linq;
+using Ets.SingleApi.Model;
+using Ets.SingleApi.Model.Repository;
+using Ets.SingleApi.Model.Services;
+using Ets.SingleApi.Services.IRepository;
+using Ets.SingleApi.Utility;
+
+namespace Ets.SingleApi.Services.Order
+{
+    /// <summary>
+    /// 类名称：TangShiOrderBaseProvider
+    /// 命名空间：Ets.SingleApi.Services.Order
+    /// 类功能：
+    /// </summary>
+    /// 创建者：单琪彬
+    /// 创建日期：2/28/2014 4:25 PM
+    /// 修改者：
+    /// 修改时间：
+    /// ----------------------------------------------------------------------------------------
+    public class TangShiOrderBaseProvider : IOrderBaseProvider
+    {
+        /// <summary>
+        /// 字段orderNumberDtEntityRepository
+        /// </summary>
+        /// 创建者：周超
+        /// 创建日期：10/25/2013 2:14 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        private readonly INHibernateRepository<OrderNumberDtEntity> orderNumberDtEntityRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DingTaiOrderBaseProvider"/> class.
+        /// </summary>
+        /// <param name="orderNumberDtEntityRepository">The orderNumberDtEntityRepository</param>
+        /// 创建者：周超
+        /// 创建日期：10/25/2013 2:14 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public TangShiOrderBaseProvider(
+            INHibernateRepository<OrderNumberDtEntity> orderNumberDtEntityRepository)
+        {
+            this.orderNumberDtEntityRepository = orderNumberDtEntityRepository;
+        }
+
+
+        public OrderType OrderType
+        {
+            get { return OrderType.DingTai; }
+        }
+
+        /// <summary>
+        /// 取得一个订单号
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <returns>
+        /// 订单号
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:10 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<string> GetOrderNumber(string source)
+        {
+            var entity = this.orderNumberDtEntityRepository.EntityQueryable.FirstOrDefault();
+            if (entity == null)
+            {
+                return new ServicesResult<string>
+                {
+                    StatusCode = (int)StatusCode.General.OrderNumberNotFound,
+                    Result = string.Empty
+                };
+            }
+
+            var orderNumber = entity.OrderId;
+            this.orderNumberDtEntityRepository.Remove(entity);
+            return new ServicesResult<string>
+            {
+                Result = orderNumber.ToString()
+            };
+        }
+
+        /// <summary>
+        /// 取得订单是否存在以及支付支付状态
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单状态</param>
+        /// <returns>
+        /// 返回结果 0 不存在 1 支付 2 未支付
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:11 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<int> Exist(string source, int orderId)
+        {
+            return new ServicesResult<int>();
+        }
+
+
+        /// <summary>
+        /// 保存支付状态
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单号</param>
+        /// <param name="isPaid">支付状态</param>
+        /// <returns>
+        /// 返回结果
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:24 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<bool> SaveOrderPaid(string source, int orderId, bool isPaid)
+        {
+            return new ServicesResult<bool>();
+        }
+
+        /// <summary>
+        /// 保存订单的支付方式
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单号</param>
+        /// <param name="paymentMethodId">支付方式</param>
+        /// <param name="payBank">备注</param>
+        /// <returns>
+        /// 返回结果
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:24 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<bool> SaveOrderPaymentMethod(string source, int orderId, int paymentMethodId, string payBank)
+        {
+            return new ServicesResult<bool>();
+        }
+    }
+}
---
+++ Ets.SingleApi.Services/Order/WaiMaiOrderBaseProvider.cs
@@ -0,0 +1,234 @@
+﻿using System.Linq;
+using Ets.SingleApi.Model;
+using Ets.SingleApi.Model.Repository;
+using Ets.SingleApi.Model.Services;
+using Ets.SingleApi.Services.IRepository;
+using Ets.SingleApi.Utility;
+
+namespace Ets.SingleApi.Services
+{
+    /// <summary>
+    /// 类名称：WaiMaiOrderBaseProvider
+    /// 命名空间：Ets.SingleApi.Services.Order
+    /// 类功能：
+    /// </summary>
+    /// 创建者：单琪彬
+    /// 创建日期：2/28/2014 4:03 PM
+    /// 修改者：
+    /// 修改时间：
+    /// ----------------------------------------------------------------------------------------
+    public class WaiMaiOrderBaseProvider : IOrderBaseProvider
+    {
+        /// <summary>
+        /// 字段deliveryEntityRepository
+        /// </summary>
+        /// 创建者：周超
+        /// 创建日期：10/22/2013 8:41 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        private readonly INHibernateRepository<DeliveryEntity> deliveryEntityRepository;
+
+        /// <summary>
+        /// 字段orderNumberDcEntityRepository
+        /// </summary>
+        /// 创建者：周超
+        /// 创建日期：11/23/2013 12:13 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        private readonly INHibernateRepository<OrderNumberDcEntity> orderNumberDcEntityRepository;
+        /// <summary>
+        /// 字段paymentEntityRepository
+        /// </summary>
+        /// 创建者：周超
+        /// 创建日期：11/9/2013 10:30 AM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        private readonly INHibernateRepository<PaymentEntity> paymentEntityRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WaiMaiOrderProvider" /> class.
+        /// </summary>
+        /// <param name="deliveryEntityRepository">The deliveryEntityRepository</param>
+        /// <param name="orderNumberDcEntityRepository">The orderNumberDcEntityRepository</param>
+        /// <param name="paymentEntityRepository"></param>
+        /// 创建者：周超
+        /// 创建日期：10/22/2013 8:41 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        protected WaiMaiOrderBaseProvider(
+            INHibernateRepository<DeliveryEntity> deliveryEntityRepository,
+            INHibernateRepository<OrderNumberDcEntity> orderNumberDcEntityRepository,
+            INHibernateRepository<PaymentEntity> paymentEntityRepository)
+        {
+            this.deliveryEntityRepository = deliveryEntityRepository;
+            this.orderNumberDcEntityRepository = orderNumberDcEntityRepository;
+            this.paymentEntityRepository = paymentEntityRepository;
+        }
+
+        public OrderType OrderType
+        {
+            get { return OrderType.WaiMai; }
+        }
+
+        /// <summary>
+        /// 取得一个订单号
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <returns>
+        /// 订单号
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:10 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<string> GetOrderNumber(string source)
+        {
+            var entity = this.orderNumberDcEntityRepository.EntityQueryable.FirstOrDefault();
+            if (entity == null)
+            {
+                return new ServicesResult<string>
+                {
+                    StatusCode = (int)StatusCode.General.OrderNumberNotFound,
+                    Result = string.Empty
+                };
+            }
+
+            var orderId = entity.OrderId;
+            this.orderNumberDcEntityRepository.Remove(entity);
+
+            return new ServicesResult<string>
+            {
+                Result = orderId.ToString()
+            };
+        }
+
+        /// <summary>
+        /// 取得订单是否存在以及支付支付状态
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单状态</param>
+        /// <returns>
+        /// 返回结果 0 不存在 1 支付 2 未支付
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:11 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<int> Exist(string source, int orderId)
+        {
+            var deliveryEntity = (from entity in this.deliveryEntityRepository.EntityQueryable
+                                  where entity.OrderNumber == orderId
+                                  select entity).FirstOrDefault();
+
+            if (deliveryEntity == null)
+            {
+                return new ServicesResult<int>
+                {
+                    StatusCode = (int)StatusCode.Validate.InvalidOrderIdCode
+                };
+            }
+
+            return new ServicesResult<int>
+            {
+                StatusCode = (int)StatusCode.Succeed.Ok,
+                Result = deliveryEntity.IsPaId == true ? 1 : 2
+            };
+        }
+
+        /// <summary>
+        /// 保存支付状态
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单号</param>
+        /// <param name="isPaid">支付状态</param>
+        /// <returns>
+        /// 返回结果
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:15 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<bool> SaveOrderPaid(string source, int orderId, bool isPaid)
+        {
+            var deliveryEntity = this.deliveryEntityRepository.FindSingleByExpression(p => p.OrderNumber == orderId);
+            if (deliveryEntity == null)
+            {
+                return new ServicesResult<bool>
+                {
+                    StatusCode = (int)StatusCode.Validate.InvalidOrderIdCode,
+                    Result = false
+                };
+            }
+
+            deliveryEntity.IsPaId = true;
+            this.deliveryEntityRepository.Save(deliveryEntity);
+
+            return new ServicesResult<bool>
+            {
+                StatusCode = (int)StatusCode.Succeed.Ok,
+                Result = true
+            };
+        }
+
+        /// <summary>
+        /// 保存订单的支付方式
+        /// </summary>
+        /// <param name="source">应用来源</param>
+        /// <param name="orderId">订单号</param>
+        /// <param name="paymentMethodId">支付方式</param>
+        /// <param name="payBank">备注</param>
+        /// <returns>
+        /// 返回结果
+        /// </returns>
+        /// 创建者：单琪彬
+        /// 创建日期：2/28/2014 4:15 PM
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public ServicesResult<bool> SaveOrderPaymentMethod(string source, int orderId, int paymentMethodId, string payBank)
+        {
+            var deliveryEntity = this.deliveryEntityRepository.FindSingleByExpression(p => p.OrderNumber == orderId);
+            if (deliveryEntity == null)
+            {
+                return new ServicesResult<bool>
+                {
+                    StatusCode = (int)StatusCode.Validate.InvalidOrderIdCode,
+                    Result = false
+                };
+            }
+            var paymentEntity = this.paymentEntityRepository.EntityQueryable.FirstOrDefault(p => p.Delivery.DeliveryId == deliveryEntity.DeliveryId)
+                                 ?? new PaymentEntity
+                                 {
+                                     PaymentTypeId = 1,
+                                     Delivery = new DeliveryEntity { DeliveryId = deliveryEntity.DeliveryId },
+                                     TransactionCode = string.Empty,
+                                     CardId = string.Empty,
+                                     Authorized = false,
+                                     CVV = 0,
+                                     AVS = string.Empty,
+                                     PC = 0,
+                                     AuthCode = string.Empty,
+                                     VoicePayResponse = string.Empty,
+                                     TransactionFee = 0
+                                 };
+
+            paymentEntity.Amount = deliveryEntity.CustomerTotal ?? 0;
+            paymentEntity.MethodChangeHistory = paymentEntity.PaymentMethodId.ToString();
+            paymentEntity.PaymentMethodId = paymentMethodId;
+            paymentEntity.PayBank = payBank;
+            this.paymentEntityRepository.Save(paymentEntity);
+            return new ServicesResult<bool>
+            {
+                StatusCode = (int)StatusCode.Succeed.Ok,
+                Result = true
+            };
+        }
+    }
+}
