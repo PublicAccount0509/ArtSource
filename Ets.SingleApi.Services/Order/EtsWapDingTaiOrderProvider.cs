74466a355704beda1912b9c3cc4b92d463520580
wangwei <wang.wei@etaoshi.com>
2014-03-28 20:33:06 +0800

    订单明细
    增加 显示押金和最低消费

--- Ets.SingleApi.Services/Order/EtsWapDingTaiOrderProvider.cs
+++ Ets.SingleApi.Services/Order/EtsWapDingTaiOrderProvider.cs
@@ -242,7 +242,8 @@ namespace Ets.SingleApi.Services
                                        MaxNumber = deskBookingEntity.DeskType.MaxNumber,
                                        MinNumber = deskBookingEntity.DeskType.MinNumber,
                                        TblTypeName = deskBookingEntity.DeskType.TableType.TblTypeName,
-                                       LowCost = deskBookingEntity.DeskType.LowCost
+                                       LowCost = deskBookingEntity.DeskType.LowCost,
+                                       DepositAmount = deskBookingEntity.DeskType.DepositAmount
                                    }
                               ).FirstOrDefault();
 
@@ -322,6 +323,7 @@ namespace Ets.SingleApi.Services
                 Coupon = coupon.ToString("#0.00"),//折扣
                 CustomerTotal = customerTotal.ToString("#0.00"),//总价 折扣后
                 Total = total.ToString("#0.00"),//总价 未折扣
+                DingTaiMethodId = (tableReservationEntity.Type ?? 2),//2 只订位，3 订位并点菜
                 Desk = dingTaiOrderDesk
             };
 
