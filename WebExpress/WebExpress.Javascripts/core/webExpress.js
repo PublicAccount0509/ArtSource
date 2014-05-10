779646cd6fb38b99e8a7c8a4c622a971b6484f39
wgj <402725796@qq.com>
2014-05-10 17:55:48 +0800

    Signed-off-by: wgj <402725796@qq.com>

--- WebExpress/WebExpress.Javascripts/core/webExpress.js
+++ WebExpress/WebExpress.Javascripts/core/webExpress.js
@@ -1,1 +1,12 @@
 ﻿window.webExpress = {};
+webExpress.config = {};
+webExpress.config.enums = {};
+
+function EnumItem(value,text) {
+    this.value = value;
+    this.text = text;
+}
+
+EnumItem.prototype.toString = function () {
+    return this.value;
+}
