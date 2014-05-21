58b26ee391eac6038c4c76f8bc15011c37862004
okaywang <okaywang@qq.com>
2014-05-21 16:02:15 +0900

    Signed-off-by: okaywang <okaywang@qq.com>

--- DataAccess/Art.Data.Common/Degree.cs
+++ DataAccess/Art.Data.Common/Degree.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebExpress.Core;
 
 namespace Art.Data.Common
 {
     public enum Degree
     {
+        [DisplayText("大专")]
         大专,
+
+        [DisplayText("本科")]
         本科,
+
+        [DisplayText("研究生")]
         研究生,
+
+        [DisplayText("博士")]
         博士,
+
+        [DisplayText("海归")]
         海归
     }
 }
