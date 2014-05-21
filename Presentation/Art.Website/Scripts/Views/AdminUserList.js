4a1afd2e5458a099456f53bf73b2eb26af8ae4a5
黄磊 <huang.lei@xinlong.com>
2014-05-21 17:24:25 +0800

    系统设置

--- Presentation/Art.Website/Scripts/Views/AdminUserList.js
+++ Presentation/Art.Website/Scripts/Views/AdminUserList.js
@@ -14,6 +14,9 @@
             _self.refresh = refresh;
             _self.search = search;
             _self.updateAdminUser = updateAdminUser;
+            _self.resetPassword = resetPassword;
+            _self.addAdminUser = addAdminUser;
+            _self.deleteAdminUser = deleteAdminUser;
             //_self.auctionRefuse = auctionRefuse;
         }
 
@@ -27,8 +30,78 @@
             };
             refresh();
         }
-        function updateAdminUser() {
-            alert("update");
+        function updateAdminUser(model) {
+            var url = "/AdminUser/Edit" ;
+            webExpress.utility.ajax.request(url, model,
+            function (response) {
+                if (response.IsSuccess) {
+                    alert("保存成功");
+                    refresh();
+                }
+                else {
+                    alert(response.Message);
+                }
+            });
+        }
+        function addAdminUser(model) {
+            var url = "/AdminUser/Add";
+            webExpress.utility.ajax.request(url, model,
+            function (response) {
+                if (response.IsSuccess) {
+                    alert("保存成功");
+                    refresh();
+                }
+                else {
+                    alert(response.Message);
+                }
+            });
+        }
+        //function checkPassword(userid, password) {
+        //    var checkpasswordurl = "/AdminUser/CheckPassword";
+        //    webExpress.utility.ajax.request(checkpasswordurl, { Id: userid, PassWord: password },
+        //        function(data) {
+        //            alert(data.IsSuccess);
+        //            if (!data.IsSuccess) {
+        //                $("#txtypassworderror").show();
+        //                $("#J_ypassword").focus();
+        //            }
+        //        });
+        //}
+        function deleteAdminUser(id) {
+            alert("delete " + id);
+            var url = "/AdminUser/Delete";
+            webExpress.utility.ajax.request(url, {id : id },
+            function (response) {
+                if (response.IsSuccess) {
+                    alert("删除成功");
+                    refresh();
+                }
+                else {
+                    alert(response.Message);
+                }
+            });
+        }
+        function resetPassword(model) {
+            var checkpasswordurl = "/AdminUser/CheckPassword";
+            var resetPasswordurl = "/AdminUser/ResetPassword";
+            webExpress.utility.ajax.request(checkpasswordurl, { Id: model.Id, PassWord: model.Password },
+                function(data) {
+                    if (!data.IsSuccess) {
+                        $("#txtypassworderror").text("原密码错误");
+                        $("#txtypassworderror").show();
+                        $("#J_ypassword").focus();
+                    } else {
+                        webExpress.utility.ajax.request(resetPasswordurl, model,
+                            function(response) {
+                                if (response.IsSuccess) {
+                                    alert("保存成功");
+                                    refresh();
+                                } else {
+                                    alert(response.Message);
+                                }
+                            });
+                    }
+                });
         }
         function refresh() {
             var url = "/AdminUser/List";
