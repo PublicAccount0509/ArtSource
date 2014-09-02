5a7aa2f76017ed4c970a3cc7a26ee81a2fcb9caf
qizhou.meng <22948774@qq.com>
2014-09-02 09:34:05 +0800

    modify

--- Ets.SingleApi.Controllers/UserController.cs
+++ Ets.SingleApi.Controllers/UserController.cs
@@ -987,10 +987,8 @@
                                                                    OrderStatus = orderStatus,
                                                                    PaidStatus = paidStatus,
                                                                    SupplierId = supplierId == 0 ? null : supplierId,
-                                                                   SupplierGroupId =
-                                                                       supplierGroupId == 0 ? null : supplierGroupId,
-                                                                   IsEtaoshi =
-                                                                       supplierGroupId == 17 ? false : this.IsEtaoshi,
+                                                                   SupplierGroupId =supplierGroupId == 0 ? null : supplierGroupId,
+                                                                   IsEtaoshi = supplierGroupId == 17 ? false : this.IsEtaoshi,
                                                                    PageIndex = pageIndex,
                                                                    PageSize = pageSize,
                                                                    Cancelled = cancelled,
